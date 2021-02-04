a38f3cb6a75635f0713979e93c1b75a94b746530
Dmitry <panteliymas@outlook.com>
2021-02-04 20:21:20 +0200

    all/

--- 4/main.js
+++ 4/main.js
@@ -3,24 +3,6 @@ $(document).ready(() => {
 
     function set_back(id) {
         let body = $('body');
-        // let img = document.createElement('img');//$("<img>");
-        // let back = $('#' + id);
-        // let url = back.css('background-image').split('"')[1];
-        //
-        // //alert(url);
-        // $(img).attr('src', url);
-        // let img_ratio = 0;
-        // document.body.appendChild(img);
-        // let w = $(img).width();
-        // let h = $(img).height();
-        // img.onload = function(){
-        //     w = $(this).width();
-        //     h = $(this).height();
-        //     img_ratio = h / w;
-        //     back.height(body.width() * img_ratio);
-        //     back.css('background-size', body.width() + 'px ' + (body.width() * h/w) + 'px');
-        //     $(img).remove();
-        // };
 
         let w = $('#man').width();
         $('#man').height(w);
